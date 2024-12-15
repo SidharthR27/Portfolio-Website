7e1f96f3972dc96e573481d029927de8df0f7dc7
SidharthR27 <theroyalguy7@gmail.com>
2024-12-15 16:38:59 +0530

    testing fix

--- script.js
+++ script.js
@@ -55,7 +55,7 @@ const viewWorks =(end) => {
     heading5.className = 'neetocode-link';
     const link = document.createElement('a');
     // link.href = `/projects/${item.title}/index.html`
-    link.href = `#`
+    link.href = `/Projects/Wordle/`;
     link.textContent = 'View Now';
     heading5.appendChild(link);
     workContent.appendChild(heading5);
